8964c3e0af73cb27e61adb47ff27d0f07c1664d3
realoli <connieorioli44@gmail.com>
2024-10-18 03:13:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x6CFa80FFa0d2d5a0053FAdEAC5cD0c1a226E8174";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
